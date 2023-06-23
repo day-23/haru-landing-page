1abb30d50ba4032a60605529eb58c4240b9c7f95
cjeongmin <cjeongmin27@gmail.com>
2023-06-24 03:41:57 +0900

    feature: prettierrc 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
 import styles from "../styles/Home.module.css";
 
 const HomePage = () => {
-    return(
-    <div className={styles.container}>
+  return (
+    <>
+      <style jsx>{`
+        .container {
+          background: linear-gradient(to right, #d2d7ff, #aad7ff);
+          background-clip: initial;
+          -webkit-background-clip: text;
+          -webkit-text-fill-color: transparent;
+          user-select: all;
+          -webkit-user-select: none;
+          font-size: 32px;
+        }
+
+        .container h1 {
+          font-family: "Pretendard", sans-serif;
+        }
+      `}</style>
+
+      <div className="container">
         <h1>Hello, Haru is comming soon.</h1>
-    </div>)
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
